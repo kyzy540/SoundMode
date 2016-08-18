a9e7cac0ab4ed78de026d5f92140f0184871876c
chendongqing <kyzy540@gmail.com>
2016-08-18 14:37:56 +0800

    add comments

--- src/main/java/kyzy5/soundmode/SoundModeWidget.java
+++ src/main/java/kyzy5/soundmode/SoundModeWidget.java
@@ -14,6 +14,7 @@ import android.widget.RemoteViews;
  */
 public class SoundModeWidget extends AppWidgetProvider {
 
+    // action registered for click event. It is referenced in AndroidManifest.xml <intent-filter>
     private static final String SOUND_MODE_WIDGET_CLICK = "SOUND_MODE_WIDGET_CLICK";
 
     @Override
@@ -21,6 +22,7 @@ public class SoundModeWidget extends AppWidgetProvider {
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.sound_mode_widget);
         ComponentName watchWidget = new ComponentName(context, SoundModeWidget.class);
 
+        // register click event
         Intent intent = new Intent(SOUND_MODE_WIDGET_CLICK);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         remoteViews.setOnClickPendingIntent(R.id.imageButton, pendingIntent);
@@ -35,22 +37,28 @@ public class SoundModeWidget extends AppWidgetProvider {
             AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
             if (am.getRingerMode() == AudioManager.RINGER_MODE_SILENT
                     && am.getStreamVolume(AudioManager.STREAM_RING) == 0) {
+                // quit Priority mode
                 am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                // set ring volume to max
                 am.setStreamVolume(AudioManager.STREAM_RING, 100, AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
                 remoteViews.setTextViewText(R.id.textView, "Sound");
                 remoteViews.setImageViewResource(R.id.imageButton, R.drawable.circle);
             } else if (am.getRingerMode() == AudioManager.RINGER_MODE_VIBRATE) {
+                // after Android 5.0, RINGER_MODE_SILENT is Priority mode actually
                 am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                // turn off ring tone
                 am.setStreamVolume(AudioManager.STREAM_RING, 0,  AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
                 remoteViews.setTextViewText(R.id.textView, "Priority");
                 remoteViews.setImageViewResource(R.id.imageButton, R.drawable.cross);
             } else {
+                // dummy code. Setting ring volume to 0 will automatically change ring mode to vibrate
                 am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
                 am.setStreamVolume(AudioManager.STREAM_RING, 0,  AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
                 remoteViews.setTextViewText(R.id.textView, "Vibrate");
                 remoteViews.setImageViewResource(R.id.imageButton, R.drawable.triangle);
             }
 
+            // kick off widget layout update
             ComponentName widget = new ComponentName(context, SoundModeWidget.class);
             AppWidgetManager.getInstance(context).updateAppWidget(widget, remoteViews);
         }
